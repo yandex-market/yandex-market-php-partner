30d3e1aca17fcc600aa9e1b6d5963dd54243b7b7
Vitalia Smahlenko <vitalia.smahlenko@nixsolutions.com>
2019-09-25 20:25:02 +0300

    Add client for feedback

---
+++ src/Yandex/Market/Partner/Clients/FeedbackClient.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Yandex\Market\Partner\Clients;
+
+use Yandex\Market\Partner\Models\Response\GetFeedbackResponse;
+
+class FeedbackClient extends Client
+{
+    /**
+     * Returns new and updated feedbacks
+     *
+     * @see https://yandex.ru/dev/market/partner/doc/dg/reference/get-campaigns-id-feedback-updates-docpage/
+     *
+     * @param $campaignId
+     * @param array $params
+     * @return GetFeedbackResponse
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     * @throws \Yandex\Common\Exception\ForbiddenException
+     * @throws \Yandex\Common\Exception\UnauthorizedException
+     * @throws \Yandex\Market\Partner\Exception\PartnerRequestException
+     */
+    public function getFeedback($campaignId, array $params = [])
+    {
+        $resource = 'campaigns/' . $campaignId . '/feedback/updates.json';
+        $resource .= '?' . $this->buildQueryString($params);
+        $response = $this->sendRequest('GET', $this->getServiceUrl($resource));
+        $decodedResponseBody = $this->getDecodedBody($response->getBody());
+
+        return new GetFeedbackResponse($decodedResponseBody);
+
+    }
+}
