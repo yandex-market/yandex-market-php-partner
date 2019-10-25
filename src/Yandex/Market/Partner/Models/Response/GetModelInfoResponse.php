460b713b5e9ce88c595c576f460b88687df1e6bc
Vitalia Smahlenko <vitalia.smahlenko@nixsolutions.com>
2019-10-25 12:35:32 +0300

    Add pager

--- src/Yandex/Market/Partner/Models/Response/GetModelInfoResponse.php
+++ src/Yandex/Market/Partner/Models/Response/GetModelInfoResponse.php
@@ -3,6 +3,7 @@
 namespace Yandex\Market\Partner\Models\Response;
 
 use Yandex\Common\Model;
+use Yandex\Market\Partner\Models\Common\Pager;
 use Yandex\Market\Partner\Models\MarketModels;
 
 class GetModelInfoResponse extends Model
@@ -10,9 +11,11 @@ class GetModelInfoResponse extends Model
     protected $models;
     protected $currency;
     protected $regionId;
+    protected $pager;
 
     protected $mappingClasses = [
         'models' => MarketModels::class,
+        'pager' => Pager::class,
     ];
 
     /**
@@ -37,5 +40,13 @@ class GetModelInfoResponse extends Model
     public function getRegionId()
     {
         return $this->regionId;
+    }
+
+    /**
+     * @return Pager|null
+     */
+    public function getPager()
+    {
+        return $this->pager;
     }
 }
