15100e7e6bcc49de4fdb2121d057a828f85412d3
Vitalia Smahlenko <vitalia.smahlenko@nixsolutions.com>
2019-10-28 15:42:05 +0200

    Fixes response

--- src/Yandex/Market/Partner/Models/Response/GetRecommendedBidsResponse.php
+++ src/Yandex/Market/Partner/Models/Response/GetRecommendedBidsResponse.php
@@ -2,11 +2,51 @@
 
 namespace Yandex\Market\Partner\Models\Response;
 
+use Yandex\Common\Model;
 use Yandex\Market\Partner\Models\Bids;
+use Yandex\Market\Partner\Models\Common\Errors;
 
-class GetRecommendedBidsResponse extends GetBidsResponse
+class GetRecommendedBidsResponse extends Model
 {
-    protected $propNameMap = [
-        'recommendations' => 'bids'
+    protected $status;
+    protected $errors;
+    protected $recommendations;
+
+    protected $mappingClasses = [
+        'recommendations' => Bids::class,
+        'errors' => Errors::class,
     ];
+
+    public function __construct($data = [])
+    {
+        $this->status = $data['status'];
+        if(isset($data['errors'])) {
+            $this->errors = $data['errors'];
+        }
+        parent::__construct($data['result']);
+    }
+
+    /**
+     * @return Bids
+     */
+    public function getBids()
+    {
+        return $this->recommendations;
+    }
+
+    /**
+     * @return string
+     */
+    public function getStatus()
+    {
+        return $this->status;
+    }
+
+    /**
+     * @return Errors|null
+     */
+    public function getErrors()
+    {
+        return $this->errors;
+    }
 }
